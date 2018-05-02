2883014d040f341a8af39577563167d0d3f43565
Guruprasad Thorvey <gthorvey@gmail.com>
2018-05-02 19:02:12 +0530

    fix

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,7 +10,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number): Observable<any> {
-    return this._http.get(this._albumUrl).map((response: Response) => response.json());
+  getAlbum(id: number): any {
+    this._http.get(this._albumUrl).map((response: Response) => response.json());
   }
 }
